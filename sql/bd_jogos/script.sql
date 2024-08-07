15a10d4b55454ce8127c8cceaae614e237adcbd1
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-08-07 16:23:03 -0300

    Creating databse games in sql

---
+++ sql/bd_jogos/script.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE gerenciamento_de_jogos;
+
+-- Tabelas das entidades
+
+CREATE TABLE jogador();
+
+CREATE TABLE historico();
+
+CREATE TABLE partida();
+
+CREATE TABLE ranking();
+
+CREATE TABLE jogo();
+
+CREATE TABLE eletronico();
+
+CREATE TABLE atividade_ar_livre();
+
+CREATE TABLE conquista();
+
+CREATE TABLE recompensa();
+
+-- Tabelas associativas
+
+CREATE TABLE partida_jogador();
+
+CREATE TABLE jogador_ranking();
