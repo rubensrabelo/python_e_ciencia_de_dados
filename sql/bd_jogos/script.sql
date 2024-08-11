dd88a7dfeec00ff20328acd280298c6a5333ba54
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-08-11 08:11:14 -0300

    Editing games database in SQL

--- sql/bd_jogos/script.sql
+++ sql/bd_jogos/script.sql
@@ -1,12 +1,13 @@
--- Criando e definindo o esquema
-
-CREATE SCHEMA gerenciamento_de_jogos;
-
-SET SCHEMA 'gerenciamento_de_jogos';
-
 -- Banco de dados:
 
-CREATE DATABASE jogos;
+CREATE DATABASE gerenciamento_de_jogos;
+
+-- Criando e definindo o esquema
+
+CREATE SCHEMA bd_jogos;
+
+SET SCHEMA 'bd_jogos';
+
 
 -- Tabelas das entidades:
 
@@ -28,7 +29,7 @@ CREATE TABLE historico(
 CREATE TABLE partida(
     id SERIAL PRIMARY KEY,
     data_hora TIMESTAMP NOT NULL,
-    duracao REAL,
+    duracao REAL
 );
 
 CREATE TABLE ranking(
@@ -57,7 +58,7 @@ CREATE TABLE atividade_ar_livre(
 
 CREATE TABLE conquista(
     id SERIAL PRIMARY KEY,
-    nome VARCHAR(100),
+    nome VARCHAR(100) NOT NULL,
     descricao TEXT
 );
 
@@ -79,36 +80,52 @@ ALTER TABLE atividade_ar_livre
 -- Tabelas associativas para relacionamentos n..n:
 
 CREATE TABLE partida_jogador(
-    partida_id INTEGER,
-    jogador_id INTEGER,
+    partida_id INTEGER NOT NULL,
+    jogador_id INTEGER NOT NULL,
     pontuacao_alcancada REAL NOT NULL,
-    PRIMARY KEY(partida_id, jogador_id)
+    PRIMARY KEY(partida_id, jogador_id),
+    FOREIGN KEY(partida_id) REFERENCES partida(id),
+    FOREIGN KEY(jogador_id) REFERENCES jogador(id)
 );
 
 CREATE TABLE jogador_ranking(
-    jogador_id INTEGER,
-    ranking_id INTEGER,
+    jogador_id INTEGER NOT NULL,
+    ranking_id INTEGER NOT NULL,
     posicao INTEGER NOT NULL,
-    PRIMARY KEY(jogador_id, ranking_id)
+    PRIMARY KEY(jogador_id, ranking_id),
+    FOREIGN KEY(ranking_id) REFERENCES ranking(id),
+    FOREIGN KEY(jogador_id) REFERENCES jogador(id)
 );
 
 CREATE TABLE partida_historico(
-    partida_id INTEGER,
-    historico_id INTEGER,
-    pontuacao_alcancada REAL NOT NULL
+    partida_id INTEGER NOT NULL,
+    historico_id INTEGER NOT NULL,
+    pontuacao_alcancada REAL NOT NULL,
+    PRIMARY KEY(partida_id, historico_id),
+    FOREIGN KEY(partida_id) REFERENCES partida(id),
+    FOREIGN KEY(historico_id) REFERENCES historico(id)
 );
 
 CREATE TABLE historico_conquista(
-    historico_id INTEGER,
-    conquista_id INTEGER
+    historico_id INTEGER NOT NULL,
+    conquista_id INTEGER NOT NULL,
+    PRIMARY KEY(historico_id, conquista_id),
+    FOREIGN KEY(historico_id) REFERENCES historico(id),
+    FOREIGN KEY(conquista_id) REFERENCES conquista(id)
 );
 
 CREATE TABLE ranking_conquista(
-    conquista_id INTEGER,
-    recompensa_id INTEGER
+    conquista_id INTEGER NOT NULL,
+    ranking_id INTEGER NOT NULL,
+    PRIMARY KEY(conquista_id, ranking_id),
+    FOREIGN KEY(conquista_id) REFERENCES conquista(id),
+    FOREIGN KEY(ranking_id) REFERENCES ranking(id)
 );
 
 CREATE TABLE conquista_recompensa(
-    conquista_id INTEGER,
-    recompensa_id INTEGER
-)
+    conquista_id INTEGER NOT NULL,
+    recompensa_id INTEGER NOT NULL,
+    PRIMARY KEY(conquista_id, recompensa_id),
+    FOREIGN KEY(conquista_id) REFERENCES conquista(id),
+    FOREIGN KEY(recompensa_id) REFERENCES recompensa(id)
+);
