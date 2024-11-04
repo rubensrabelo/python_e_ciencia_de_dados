a14e516dba907020379e745f54a5a3993d3770e6
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-04 20:43:16 -0300

    Studying fastapi

--- fastapi/studies/main.py
+++ fastapi/studies/main.py
@@ -1,19 +1,13 @@
 from typing import Union
 
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
-@app.post("/items")
-async def create_item(item: Item):
-    return item
+@app.get("/items/")
+async def read_items(q: Union[str, None] = Query(default=None, min_length=3, max_length=50)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
