d30b6b10f004e718f02d97317b350a691f89de55
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-05 16:43:19 -0300

    Studying fastapi

--- fastapi/studies/main.py
+++ fastapi/studies/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI, status
+from typing import Set, Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/", status_code=status.HTTP_200_OK)
-async def home():
-    return {"message": "hello"}
+class Image(BaseModel):
+    url: str
+    name: str
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    tags: Set[str] = set()
+    image: Union[Image, None] = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
