9d0ced3c28fe7b1d81060e0d4e3ce2fface22b1d
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-05 09:56:43 -0300

    First Program

--- fastapi/studies/main.py
+++ fastapi/studies/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 async def home():
     return {"message": "hello"}
