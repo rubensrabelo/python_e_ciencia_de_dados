b01bf5c7548943365f33e473d8c129af0aeb4556
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-07 11:00:04 -0300

    Finishing book project part 2

--- fastapi/first_program/main.py
+++ fastapi/first_program/main.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 from fastapi import FastAPI, Path, Query, HTTPException, Body
 from pydantic import BaseModel, Field
@@ -32,6 +32,18 @@ class BookRequest(BaseModel):
     rating: int = Field(gt=0, lt=6)
     published_date: int = Field(gt=1999, lt=2031)
 
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new book",
+                "author": "codingwithroby",
+                "description": "A new description of a book",
+                "rating": 5,
+                'published_date': 2029
+            }
+        }
+    }
+
 
 BOOKS = [
     Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5, 2030),
@@ -42,57 +54,64 @@ BOOKS = [
     Book(6, 'HP3', 'Author 3', 'Book Description', 1, 2026)
 ]
 
-# Finalizei aqui
 
-
-@app.get("/books")
+@app.get("/books", status_code=status.HTTP_200_OK)
 async def read_all_books():
     return BOOKS
 
 
-@app.get("/books/{book_title}")
-async def read_book(book_title: str):
+@app.get("/books/{book_id}", status_code=status.HTTP_200_OK)
+async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
-        if book_title.casefold() == book.get("title").casefold():
+        if book_id == book.id:
             return book
-    return {"message": "The book is not registered in the database"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
 
-@app.get("/books/by-category/")
-async def read_category_by_query(category: str):
-    books_to_return = []
+@app.get("/books/ratings/", status_code=status.HTTP_200_OK)
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
+    books_to_return: List[BookRequest] = []
     for book in BOOKS:
-        if category.casefold() == book.get("category").casefold():
+        if book.rating == book_rating:
             books_to_return.append(book)
     return books_to_return
 
 
-@app.get("/books/by-author/")
-async def read_books_by_author_path(author: str):
-    books_to_return = []
+@app.get("/books/publish-date/", status_code=status.HTTP_200_OK)
+async def red_book_by_publish_date(published_date: int = Query(gt=1999, lt=2031)):
+    books_to_return: List[BookRequest] = []
     for book in BOOKS:
-        if author.casefold() == book.get("author").casefold():
+        if book.rating == published_date:
             books_to_return.append(book)
     return books_to_return
 
 
-@app.post("/books/create-book")
-async def create_book(book=Body()):
-    BOOKS.append(book)
+@app.post("/books/create-book", status_code=status.HTTP_201_CREATED)
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request)
+    BOOKS.append(find_book_id(new_book))
+    return new_book
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
     return book
 
 
-@app.put("/books/update-book")
-async def update_book(book=Body()):
+@app.put("/books/update-book", status_code=status.HTTP_200_OK)
+async def update_book(book_request: BookRequest):
     for i in range(len(BOOKS)):
-        if BOOKS[i].get("title").casefold() == book.get("title").casefold():
-            BOOKS[i] = book
-    return book
+        if book_request.id == BOOKS[i].id:
+            BOOKS[i] = book_request
+            return book_request
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
 
-@app.delete("/books/delete-book/{book_title}")
-async def delete_book(book_title: str):
+@app.delete("/books/delete-book/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int = Path(gt=0)):
     for i in range(len(BOOKS)):
-        if BOOKS[i].get("title").casefold() == book_title.casefold():
+        if book_id == BOOKS[i].id:
             BOOKS.pop(i)
             break
+            return None
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
