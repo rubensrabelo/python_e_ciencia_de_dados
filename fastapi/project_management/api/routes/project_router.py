6ac864baea886315e654a36affacd9440c82b50c
SammyFerreiraa <sammyfe2021@gmail.com>
2024-11-16 11:58:20 -0300

    :bug: fix: handle missing optional field in GET request

--- fastapi/project_management/api/routes/project_router.py
+++ fastapi/project_management/api/routes/project_router.py
@@ -17,7 +17,7 @@ class ProjectRequest(BaseModel):
     name: str = Field(..., min_length=3, max_length=100)
     description: str = Field(..., max_length=250)
     start_date: date = Field(...)
-    end_date: Optional[date] = Field(default=None)
+    end_date: Optional[date] = Field(default="undefined")
     completion_prediction: date = Field(...)
     status: str = Field(default="Planned")
 
