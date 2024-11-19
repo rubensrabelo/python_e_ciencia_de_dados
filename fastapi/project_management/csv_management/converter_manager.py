4783ca6244db752e6ab232e92935f0265ca79dea
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-19 12:29:29 -0300

    Fixing converter and hash managers

--- fastapi/project_management/csv_management/converter_manager.py
+++ fastapi/project_management/csv_management/converter_manager.py
@@ -1,6 +1,5 @@
 import os
 import zipfile
-from fastapi import HTTPException, status
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -11,11 +10,5 @@ ZIP_FILE = os.path.join(BASE_DIR, "files", "db.zip")
 class ZipManager:
     @staticmethod
     def csv_to_zip():
-        if not os.path.exists(CSV_FILE):
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="O arquivo CSV não foi encontrado."
-            )
-
         with zipfile.ZipFile(ZIP_FILE, "w") as file:
             file.write(CSV_FILE, arcname=os.path.basename(CSV_FILE))
--- fastapi/project_management/csv_management/hash_manager.py
+++ fastapi/project_management/csv_management/hash_manager.py
@@ -1,6 +1,5 @@
 import os
 import hashlib
-from fastapi import HTTPException, status
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -10,12 +9,6 @@ CSV_FILE = os.path.join("db", "db.csv")
 class HashManager:
     @staticmethod
     def generate_hash256():
-        if not os.path.exists(CSV_FILE):
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="O arquivo CSV não foi encontrado."
-            )
-
         sha256_hash = hashlib.sha3_256()
         with open(CSV_FILE, "rb") as file:
             for byte_block in iter(lambda: file.read(4096), b""):
