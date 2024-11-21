03118e462cd898e51ef25e10034d54da7e77f39f
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-21 07:54:49 -0300

    Editing controller

--- fastapi/project_management/api/controller.py
+++ fastapi/project_management/api/controller.py
@@ -6,7 +6,9 @@ from .routes.hash_router import router as hash_router
 
 api_router = APIRouter()
 
-api_router.include_router(project_router, prefix="/projects", tags=["Projects"])
-api_router.include_router(static_router, prefix="/static", tags=["Quantity"])
+api_router.include_router(project_router, prefix="/projects",
+                          tags=["Projects"])
+api_router.include_router(static_router, prefix="/statistic",
+                          tags=["Quantity"])
 api_router.include_router(converter_router, prefix="/converter", tags=["Zip"])
 api_router.include_router(hash_router, prefix="/hash", tags=["Hash"])
