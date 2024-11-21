b90e98f90f7bd558897d315d07a7430ccc0b3f50
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-21 10:20:29 -0300

    Fixing url of the hash_router

--- fastapi/project_management/api/routes/hash_router.py
+++ fastapi/project_management/api/routes/hash_router.py
@@ -6,7 +6,7 @@ from csv_management.hash_manager import HashManager
 router = APIRouter()
 
 
-@router.get("/hash256/", status_code=status.HTTP_200_OK)
+@router.get("/sha256/", status_code=status.HTTP_200_OK)
 async def get_csv_sha256():
     hash_sha256 = HashManager.generate_hash256()
     return JSONResponse(content={"sha256": hash_sha256})
