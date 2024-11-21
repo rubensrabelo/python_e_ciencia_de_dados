c680b60ed8e89adcd160bb54e2da99dc81c341f1
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-21 10:26:49 -0300

    Fixing and adding documentation in the hash_mananager

--- fastapi/project_management/csv_management/hash_manager.py
+++ fastapi/project_management/csv_management/hash_manager.py
@@ -1,16 +1,30 @@
-import os
-import hashlib
+import os  # Define o módulo para a interação com o so.
+import hashlib  # Define o módulo para gerar e manipular hashes
 
+# Obtém o diretório base do arquivo atual.
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# Estabelece Caminho para o arquivo CSV para qual o hash será gerado.
 CSV_FILE = os.path.join("db", "db.csv")
 
 
 class HashManager:
+    """
+    Classe responsável para gerar o hash SHA-256 de um arquivo
+    """
+
     @staticmethod
     def generate_hash256():
-        sha256_hash = hashlib.sha3_256()
+        """
+        Gera o hash SHA-256 do arquivo CSV especificado.
+
+        O arquivo CSV será lido em modo binário e seu conteúdo será passado
+        para o algoritmo de hash SHA-256. O valor do hash gerado será
+        retornado como uma string hexadecimal.
+        """
+
+        # Abre o arquivo CSV no modo binário ('rb') para leitura.
         with open(CSV_FILE, "rb") as file:
-            for byte_block in iter(lambda: file.read(4096), b""):
-                sha256_hash.update(byte_block)
-        return sha256_hash.hexdigest()
+            # Lê todo o conteúdo do arquivo e gera o hash SHA-256. O resultado
+            # é retornado como um valor hexadecimal.
+            return hashlib.sha256(file.read()).hexdigest()
