eacd4208818b30c18bc454b727ad2145c372088b
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-23 18:39:05 -0300

    Improving the main.py code

--- fastapi/course_project/main.py
+++ fastapi/course_project/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from http import HTTPStatus
+
+from schemas import Message
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 async def read_root():
     return {"message": "Hello world!"}
