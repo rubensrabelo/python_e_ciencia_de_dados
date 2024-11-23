00d3f439860ef9a7acc909f176bdab3a0137d83c
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-23 15:34:51 -0300

    Documenting models file

--- fastapi/project_management/models.py
+++ fastapi/project_management/models.py
@@ -1,8 +1,15 @@
+# Importa os tipos opcionais para permitir campos que podem não ser preenchidos
 from typing import Optional
+
+# Importa a classe BaseModel e Field do Pydantic para validação de dados
 from pydantic import BaseModel, Field
+
+# Importa o tipo 'date' para trabalhar com datas
 from datetime import date
 
 
+# Definição do modelo de dados para solicitação 
+# de criação/atualização de um projeto
 class ProjectRequest(BaseModel):
     id: Optional[int] = Field(default=None)
     name: str = Field(..., min_length=3, max_length=100)
