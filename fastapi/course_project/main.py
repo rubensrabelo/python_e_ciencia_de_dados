0d6976766689723393646441ec00b909660c1024
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-24 14:30:40 -0300

    Improving fastapi project in the course

--- fastapi/course_project/main.py
+++ fastapi/course_project/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 from http import HTTPStatus
 
-from schemas import Message
+from schemas import UserPublic, UserPrivate, UserDB
 
 app = FastAPI()
 
+database = []
 
-@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
-async def read_root():
-    return {"message": "Hello world!"}
+
+@app.post("/users/",
+          status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserPrivate):
+    user_with_id = UserDB(**user.model_dump(), id=len(database)+1)
+
+    database.append(user_with_id)
+
+    return user_with_id
--- fastapi/course_project/schemas.py
+++ fastapi/course_project/schemas.py
@@ -1,5 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
-class Message(BaseModel):
-    message: str
+class UserPublic(BaseModel):
+    username: str
+    email: EmailStr
+
+
+class UserPrivate(UserPublic):
+    password: str
+
+
+class UserDB():
+    id: int
